d4f23a5b66d81c409324f086407d03ae8a624816
MussaKhan74 <MarkeLoof@gmail.com>
2022-11-28 22:59:15 +0500

    generics and duck typing

--- app.ts
+++ app.ts
@@ -166,3 +166,138 @@ printId("1");
 function getFirstThree(x: string | number[]) {
   return x.slice(0, 3);
 }
+
+// Generics
+function logString(arg: string) {
+  console.log(arg);
+  return arg;
+}
+
+function logNumber(arg: number) {
+  console.log(arg);
+  return arg;
+}
+
+function logArray(arg: any[]) {
+  console.log(arg);
+  return arg;
+}
+
+// example
+function logAnything<T>(arg: T): T {
+  console.log(arg);
+  return arg;
+}
+
+logAnything([4, 3]);
+
+interface HasAge {
+  age: number;
+}
+
+function getOldest<T extends HasAge>(people: T[]): T {
+  return people.sort((a, b) => b.age - a.age)[0];
+}
+
+const people: HasAge[] = [{ age: 30 }, { age: 40 }, { age: 10 }];
+
+getOldest(people);
+
+interface Player {
+  name: string;
+  age: number;
+}
+
+const players: Player[] = [
+  { name: "John", age: 30 },
+  { name: "Jane", age: 35 },
+  { name: "Joe", age: 16 },
+];
+
+// getOldest(players).name
+
+// Assertion
+// const person = getOldest(players) as Player;
+// person.name;
+// person.age;
+
+// This issue can be solved with generics
+
+// const person = getOldest(people);
+// person.age;
+// person.name;
+
+interface IPost {
+  title: string;
+  id: number;
+  description: string;
+}
+
+const fetchPostData = async (path: string): Promise<IPost[]> => {
+  const response = await fetch(`http://example.com${path}`);
+  return response.json();
+};
+
+interface IUser {
+  id: number;
+  name: string;
+  age: number;
+}
+
+const fetchUserData = async (path: string): Promise<IUser[]> => {
+  const response = await fetch(`http://example.com${path}`);
+  return response.json();
+};
+
+const fetchData = async <ResultType>(path: string): Promise<ResultType> => {
+  const response = await fetch(`http://example.com${path}`);
+  return response.json();
+};
+
+(async () => {
+  // const posts = await fetchPostData("/posts");
+  // posts[0];
+  // const users = await fetchUserData("/users");
+  // users[0];
+  const users = await fetchData<IPost[]>("/users");
+  users[0];
+})();
+
+// Structural Typing or Duck Typing
+
+interface ICredentials {
+  username: string;
+  password: string;
+  isAdmin?: boolean;
+}
+
+function loginCredentials(credentials: ICredentials): boolean {
+  console.log(credentials);
+  return true;
+}
+
+const user2 = {
+  username: "mussa",
+  password: "secret",
+  isAdmin: true,
+};
+
+loginCredentials(user2);
+
+interface IAuth {
+  username: string;
+  password: string;
+  login(username: string, password: string): void;
+}
+
+const auth: IAuth = {
+  username: "khan",
+  password: "secret",
+  login(username: string, password: string) {
+    return true;
+  },
+};
+
+// Inferece | Under the hood
+let numnum = "";
+let numnum2 = 1;
